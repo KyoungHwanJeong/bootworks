48a3b1e5bf5c5c4a83b3b1cfc900b9d110205b13
KyoungHwanJeong <jkh3100@naver.com>
2024-01-15 18:35:21 +0900

    2024_1_15

---
+++ bootboard/src/main/java/com/khit/board/dto/MemberDTO.java
@@ -0,0 +1,27 @@
+package com.khit.board.dto;
+
+import com.khit.board.entity.Member;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+	private Long id;
+	private String memberEmail;
+	private String memberPassword;
+	private String memberName;
+	private int memberAge;
+	
+	//entity를 dto로 변환하는 메서드
+	public static MemberDTO toSaveDTO(Member member) {
+		//member에서 가져와서 get() memberDTO에 set()
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(member.getId());
+		memberDTO.setMemberEmail(member.getMemberEmail());
+		memberDTO.setMemberPassword(member.getMemberPassword());
+		memberDTO.setMemberName(member.getMemberName());
+		memberDTO.setMemberAge(member.getMemberAge());
+	
+		return memberDTO;
+	}
+}
---
+++ jwboot/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MyHome...</title>
+<link rel="stylesheet" href="static/resources/css/style.css"/>
+</head>
+<body>
+	<div class="wrap">
+		<h1>MyHome에 오신 것을 환영합니다...</h1>
+		<div>
+			<img src="/static/images/bear.jpg">
+		</div>
+		<div>
+			<a href="/board/write">글쓰기</a>&nbsp;&nbsp;
+			<a href="/board/">글목록</a>
+		</div>
+	</div>
+</body>
+</html>
